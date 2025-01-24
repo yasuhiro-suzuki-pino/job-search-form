f1bd209bc238e4581f7022abaed91e9ef3404bd3
Yasuhiro Suzuki <yasuhirosuzuki@yasuhirosuzuki3noMacBook-Pro.local>
2025-01-24 16:19:28 +0900

    Insert GTM Tag

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+// src/App.vue
 <template>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=G-LH21B7Y1MK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+  </noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <router-view></router-view>
 </template>
+
+<script>
+export default {
+  mounted() {
+    // Google Tag Manager
+    const script = document.createElement('script')
+    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','G-LH21B7Y1MK');`
+    document.head.appendChild(script)
+  }
+}
+</script>
